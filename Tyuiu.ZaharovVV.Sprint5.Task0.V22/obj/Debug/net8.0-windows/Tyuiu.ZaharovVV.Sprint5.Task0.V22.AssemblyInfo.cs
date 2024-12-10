00284489b4731df63639f9795d40121d1e8a8533
ZaharovVladimir <chibisntbshow@gmail.com>
2024-12-11 04:00:49 +0500

    22

--- Tyuiu.ZaharovVV.Sprint5.Task0.V22/obj/Debug/net8.0-windows/Tyuiu.ZaharovVV.Sprint5.Task0.V22.AssemblyInfo.cs
+++ Tyuiu.ZaharovVV.Sprint5.Task0.V22/obj/Debug/net8.0-windows/Tyuiu.ZaharovVV.Sprint5.Task0.V22.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tyuiu.ZaharovVV.Sprint5.Task0.V22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30a79fa9f8170901ae9c021a07946f13a35cdc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tyuiu.ZaharovVV.Sprint5.Task0.V22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tyuiu.ZaharovVV.Sprint5.Task0.V22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
